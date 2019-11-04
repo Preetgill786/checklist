62389d00b5cb9061f6e1871d18b3a13610122751
Preetgill786 <57011583+Preetgill786@users.noreply.github.com>
2019-11-04 12:21:01 -0500

    Add files via upload

---
+++ Checklists/ChecklistItem .swift
@@ -0,0 +1,18 @@
+//
+//  ChecklistItem .swift
+//  Checklists
+//
+//  Created by MacStudent on 2019-11-04.
+//  Copyright © 2019 Razeware. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+var text = ""
+var checked = false
+    
+    func toggleChecked() {
+      checked = !checked
+    }
+}
